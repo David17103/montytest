d0ee045856d7cdcbbdabf3e32648cd136897f53c
David Chetty <125439364+David17103@users.noreply.github.com>
2023-06-21 09:03:14 +0200

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
-  *f_sub- sustration
+  *f_sub- the sustration
   *@head: stack head
   *@counter: line_number
   *Return: no return
@@ -16,9 +16,9 @@ void f_sub(stack_t **head, unsigned int counter)
 	if (nodes < 2)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
 		free_stack(*head);
+		free(bus.content);
+		fclose(bus.file);
 		exit(EXIT_FAILURE);
 	}
 	aux = *head;
